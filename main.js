14b00753803111ff7046b75bf0dad0fbac898deb
Jose Antonio <antoniojaraujo0@gmail.com>
2023-01-17 23:34:59 -0300

    static to dynamic data

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 //NOTE - variables and event selectors
 
 const apiKey = "d00a2d1c7846aa440fc2c56553909abc";
-const urlCountryApi = "https://countryflagsapi.com/png/";
+const urlCountryApi = "https://flagsapi.com/BE/flat/64.png";
 
 const cityInput = document.getElementById("city-input");
 const searchBtn = document.getElementById("search");
 
 const cityEl = document.querySelector("#city");
-const temperatureEl = document.querySelector("#temperature span");
+const temperatureEl = document.querySelector("#temperature");
 const descriptionEl = document.querySelector("#description");
 const weatherIconEl = document.querySelector("#weather-icon");
 const countryEl = document.querySelector("#country");
-const umidityEl = document.querySelector("#umidity span");
+const humidityEl = document.querySelector("#humidity span");
 const windEl = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
 
 //NOTE - functions
 getWeather = async (city) => {
@@ -20,15 +22,37 @@ getWeather = async (city) => {
 	const response = await fetch(apiWeatherUrl);
 	const data = await response.json();
 
-	console.log(data);
+	return data;
 };
 
-const showDataHandler = (e) => {
-	e.preventDefault();
-	const city = cityInput.value;
+const showWeatherData = async (city) => {
+	const data = await getWeather(city);
 
-	getWeather(city);
+	cityEl.innerText = data.name;
+	temperatureEl.innerText = parseInt(data.main.temp);
+	descriptionEl.innerText = data.weather[0].description;
+	weatherIconEl.setAttribute(
+		"src",
+		`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+	);
+	countryEl.setAttribute(
+		"src",
+		`https://flagsapi.com/${data.sys.country}/flat/64.png`
+	);
+	humidityEl.innerText = `${data.main.humidity}%`;
+	windEl.innerText = `${data.wind.speed}km/h`;
+
+	weatherContainer.classList.remove("hide");
 };
 
 //NOTE - Events
-searchBtn.addEventListener("click", showDataHandler);
+searchBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	const city = cityInput.value;
+	city === "" ? window.alert("digite algo") : showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+	const city = e.target.value;
+	e.code === "Enter" ? showWeatherData(city) : null;
+});
